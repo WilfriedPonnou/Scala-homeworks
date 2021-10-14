5eba8b8a7f392c25ad47f77da6d611bef33492a7
WilfriedPonnou <75972109+WilfriedPonnou@users.noreply.github.com>
2021-10-14 19:54:46 +0200

    third commit

--- Scalahomework1.scala
+++ Scalahomework1.scala
@@ -2,3 +2,5 @@ sealed abstract trait Vehicule{}
 case class Car(x:C) extends Vehicule{}
 case class Motorbike(x:M) extends Vehicule{}
 case class Boat(x:B) extends Vehicule{}
+/*on choisit des case class car les classes car, motorbike et boat
+sont susceptibles de porter de l'information (Puissance, niveau carburant,etc.*/
